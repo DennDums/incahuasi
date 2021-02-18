d7f0141701bacc150198e469b6164d3ccd5ce4a3
DennDums <44644481+DennDums@users.noreply.github.com>
2021-02-18 08:39:49 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Leslie Soto S.</title>
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="css/mobile.css" media="screen and (max-width:760px)">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald:700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Bree+Serif&display=swap" rel="stylesheet">
+
+</head>
+<body>
+	<div id="JL">
+			<p id="jless">1</p>
+			<p id="jless">6</p>
+			<p id="jless">1</p>
+			<p id="jless">0</p>
+			<p id="jless">2</p>
+			<p id="jless">0</p>
+			<p id="jless">1</p>
+			<p id="jless">7</p>
+			<p id="jless">1</p>
+			<p id="jless">0</p>
+			<p id="jless">5</p>
+			<p id="jless">9</p>
+	</div>
+	<div class="wrap">
+				<div class="widget">
+			<div class="fecha">
+				<p id="diaSemana" class="diaSemana"></p>
+				<p id="dia" class="dia"></p>
+				<p>de</p>
+				<p id="mes" class="mes"></p>
+				<p>del</p>
+				<p id="year" class="year"></p>
+			</div>
+			<div class="reloj">
+				<p id="horas" class="horas"></p>
+				<p>:</p>
+				<p id="minutos" class="minutos"></p>
+				<p>:</p>
+				<div class="caja-segundos">
+					<p id="ampm" class="ampm"></p>
+					<p id="segundos" class="segundos"></p>
+				</div>
+			</div>
+		</div>
+		
+		
+		<div class="content-all">
+			<div><p id="leslie">Leslie E. Soto Santamaría</p></div>
+			<div class="content-carrousel">
+				<figure><img src="img_L/L_7.jpg"></figure>
+				<figure><img src="img_L/L_11.jpg"></figure>
+				<figure><img src="img_L/L_14.jpg"></figure>
+				<figure><img src="img_L/L_15.jpg"></figure>
+				<figure><img src="img_L/L_3.jpg"></figure>
+				<figure><img src="img_L/L_4.jpg"></figure>
+				<figure><img src="img_L/L_1.jpg"></figure>
+				<figure><img src="img_L/L_5.jpg"></figure>
+				<figure><img src="img_L/L_12.jpg"></figure>
+				<figure><img src="img_L/L_10.jpg"></figure>
+			</div>
+		</div>
+		<div id="ad08">
+				<h1>------</h1>
+			<p>
+				Cómo sacarte de mi mente <br>
+				Si el quien controla mi mente <br> 
+				No soy yo, eres tú<br> <br>
+				Cómo decirte te amo <br>
+				Si eso dicen los humanos<br>
+				Y tú, eres una Diosa.<br>
+
+				
+
+			</p>
+			<P id="lesss">A Leslie Soto</P>
+		</div>
+	</div>
+	<script src="js/reloj.js"></script>
+	<footer class="ad08">
+		<p id="ad" class="ad"><marquee behavior="alternate" direction="right">Javier J. Calderón Sánchez - BETA</marquee></p>
+	</footer>
+</body>
+</html>
